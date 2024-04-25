8acffcce47bec96c622857864ee68eb005c584cd
ssetsofia <setsofia@washnote.com>
2024-04-25 00:17:43 +0000

    first commit

---
+++ tutorial/tutorial/spiders/covid_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class CovidSpider(scrapy.Spider):
+    name ='covid'
+    allowed_domains=['www.worldometers.info']
+    start_urls=['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+        yield {
+            'country':name,
+            'country_link' : link,
+
+        }
