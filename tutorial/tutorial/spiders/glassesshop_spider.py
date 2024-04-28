fb629c07b59b5880335097f5387d12034a014de5
ssetsofia <setsofia@washnote.com>
2024-04-28 23:40:50 +0000

    glasses shop scraping

--- tutorial/tutorial/spiders/glassesshop_spider.py
+++ tutorial/tutorial/spiders/glassesshop_spider.py
@@ -1,6 +1,6 @@
 import scrapy
 
-class GlassesShopSpider (scrapy.Spider):
+class GlassesShopSpider(scrapy.Spider):
     name = 'glasses_shop'
     allowed_domains = ['www.glassesshop.com']
 
@@ -11,7 +11,9 @@ class GlassesShopSpider (scrapy.Spider):
     def parse(self, response):
         for product in response.xpath('//div[@class="row pt-lg-5 product-list column-1"]'):
             yield {
-                'Product Name' : product.xpath('.//div/div/div/div/div/div[@class="p-title"]/a/text()').get(),
-                'Product URL' : product.xpath('.//div/div/div/div/div/div[@class="p-title"]/a[1]/@href').get(),
-                'Product Price' : product.xpath('.//div/div/div/div/div/div[@class="p-price"][1]/text()[1]').get(),
+                'product' : product
+                # 'Product Name' : product.xpath('.//div[@class="p-title"]/a[1]/text()').get(),
+                # 'Product URL' : product.xpath('.//div[@class="p-title"]/a[1]/@href').get(),
+                # 'Product Price' : product.xpath('.//div[@class="p-price"][1]/div[1]/span/text()').get(),
+                # 'Product Image' : product.xpath('.//div/a[1]/img[@class="lazy d-block w-100 product-img-default"]').get(),
             }
